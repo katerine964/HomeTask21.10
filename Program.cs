7d42b2942fb48b15a89e9b1797acc480f760094a
Kate <ekatromanova90@mail.ru>
2022-10-23 23:38:07 +0300

    21.10

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+void InputMatrix(float [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next();
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+float [,] matrix = new float[n, m];
+InputMatrix(matrix);
